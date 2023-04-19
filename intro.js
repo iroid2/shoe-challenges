248f4bda1b80349a77554637498cc6421a10c52e
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-04-19 13:11:04 +0300

    .

--- intro.js
+++ intro.js
@@ -19,3 +19,55 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+const swiperBrand = new Swiper('#brand-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView:4,
+    spaceBetween: 5,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  const swiperRotate = new Swiper('#rotate-slide', {
+    effect: "cube",
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
